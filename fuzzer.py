10c3600541c464abd4f3c0be2a8cb3e1d6529441
zamieabdullah <zamieabdullah0@gmail.com>
2021-03-19 17:19:22 -0400

    Arguments setup

--- fuzzer.py
+++ fuzzer.py
@@ -1,2 +1,10 @@
 # fuzzer.py
 # Created by Zamie Abdullah
+
+import argparse
+
+parser = argparse.ArgumentParser(description='A fuzzer program that identifies Cross-Site Scripting bugs')
+parser.add_argument('-u', dest='url', help='Link that will be fuzzed by the program')
+parser.add_argument('-r', dest='fuzzlist', help='A fuzzing wordlist that will be injected into the link')
+args = parser.parse_args()
+
