c77ab0a7fd220ee133bb1bd814c83a4ab3206f5b
zamieabdullah <zamieabdullah0@gmail.com>
2021-03-19 21:35:32 -0400

    fuzzing working with user inputs

--- fuzzer.py
+++ fuzzer.py
@@ -1,10 +1,38 @@
 # fuzzer.py
 # Created by Zamie Abdullah
 
+import requests
 import argparse
 
+list = []
+
+def fuzz(target):
+    try:
+        for payload in list:
+            req = requests.post(target + payload)
+            if payload in req.text:
+                print(payload, ' has been identified in HTTP request')
+            else:
+                print(payload, ' not identified in HTTP request')
+    except:
+        print('HTTP request not available')
+    
+
 parser = argparse.ArgumentParser(description='A fuzzer program that identifies Cross-Site Scripting bugs')
-parser.add_argument('-u', dest='url', help='Link that will be fuzzed by the program')
-parser.add_argument('-r', dest='fuzzlist', help='A fuzzing wordlist that will be injected into the link')
+parser.add_argument('-u', dest='url', help='Link that will be fuzzed by the program', default = '')
+parser.add_argument('-r', dest='fuzzlist', help='A fuzzing wordlist that will be injected into the link', default = '')
 args = parser.parse_args()
 
+if args.url == '':
+    target = input('Target Link: ')
+else:
+    target = args.url
+    print('Target Link:', target)
+    
+if args.fuzzlist == '':
+    word_list = input('Input fuzz: ')
+    list.append(word_list)
+else:
+    pass
+    
+fuzz(target)
