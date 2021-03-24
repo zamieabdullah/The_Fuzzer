4a14fc796f347671756096b34a1b7c6d87c68f97
zamieabdullah <zamieabdullah0@gmail.com>
2021-03-24 13:56:33 -0400

    completed fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -1,17 +1,38 @@
 # fuzzer.py
 # Created by Zamie Abdullah
+# Date of last edit: 03/24/2021
+# 
+# Program: 
+# A fuzzing test script that identifies reflected Cross-Site Scripting (XSS) bugs.
 
 import requests
 import argparse
 
 list = []
 
+
+# Reads through a given fuzzing wordlist within the directory and appends 
+# each line into a list.
+def read_list(fuzz_list):
+    try:
+        with open(fuzz_list, 'r') as file:
+            content = file.readlines()
+        for line in content:
+            line = line.strip('\n')
+            list.append(line)
+        
+    except:
+        print('Fuzz list not found within directory')
+
+# Using a given target url, a payload from the fuzzing wordlist will be added 
+# to the end of the url, checking to see if the payload is visible when it is
+# being posted.
 def fuzz(target):
     try:
         for payload in list:
             req = requests.post(target + payload)
             if payload in req.text:
-                print(payload, ' has been identified in HTTP request')
+                print(payload + ' has been identified in HTTP request')
             else:
                 print(payload, ' not identified in HTTP request')
     except:
@@ -24,15 +45,15 @@ parser.add_argument('-r', dest='fuzzlist', help='A fuzzing wordlist that will be
 args = parser.parse_args()
 
 if args.url == '':
-    target = input('Target Link: ')
+    target = input('Target Link: ') # Asks for user to input a target link when not in argument
 else:
     target = args.url
     print('Target Link:', target)
     
 if args.fuzzlist == '':
-    word_list = input('Input fuzz: ')
+    word_list = input('Input fuzz: ') # Asks for user to input a target link when not in argument
     list.append(word_list)
 else:
-    pass
+    read_list(args.fuzzlist)
     
 fuzz(target)
